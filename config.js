8df17134e07f5f99b1e84035a21184dfb1730afc
mxzenyasser <mazenanimation5@gmail.com>
2021-10-07 19:26:43 +0200

    Weather App

---
+++ config.js
@@ -0,0 +1,7 @@
+const constants={
+    openWeatherMap:{
+        BASE_URL:"https://api.openweathermap.org/data/2.5/weather?q=",
+        SECRET_KEY:"=7235e6d3ff7146041cf2881ffb6a89dc"
+    }
+}
+module.exports=constants
---
+++ src/app.js
@@ -0,0 +1,80 @@
+//DEPSSS
+const express = require ('express')
+const hbs = require('hbs')
+const { dirname } = require('path')
+const path = require('path')
+const app = express()
+const weatherData=require('../utils/weatherData')
+
+//MIDDLEWAREEEEEE
+const publicStaticDirPath = path.join(__dirname, '../public')
+
+const viewsPath = path.join(__dirname, '../templates/views');
+
+const partialsPath = path.join(__dirname, '../templates/partials');
+
+app.set('view engine', 'hbs');
+app.set('views', viewsPath);
+hbs.registerPartials(partialsPath);
+app.use(express.static(publicStaticDirPath));
+
+
+//ROUTESSSSSSSSS
+app.get('', (req, res) => {
+    res.render('index', {
+        title: 'Weather Teller'
+    })
+})
+
+// app.get('/weather',(req,res)=>{
+//     const address=req.query.address
+
+//     weatherData(address,(error,{temperature,description,cityName})=>{
+//         if(error){
+//             return res.send({
+//                 error
+//             })
+//         }
+//         console.log(temperature,description,cityName);
+//         res.send({
+//             temperature,
+//             description,
+//             cityName
+//         })
+//     })
+// })
+//localhost:3000/weather?address=lahore
+app.get('/weather', (req, res) => {
+    const address = req.query.address
+    if(!address) {
+        return res.send({
+            error: "You must enter address in search text box"
+        })
+    }
+
+    weatherData(address, (error, {temperature, description, cityName} = {}) => {
+        if(error) {
+            return res.send({
+                error
+            })
+        }
+        console.log(temperature, description, cityName);
+        res.send({
+            temperature,
+            description,
+            cityName
+        })
+    })
+});
+app.get("*", (req, res) => {
+    res.render('404', {
+        title: "page not found"
+    })
+})
+ 
+
+//SERVER-PORT
+const port = process.env.PORT ||3000
+app.listen (port, () => {
+    console.log('Server is up and running on port:', port);
+})
